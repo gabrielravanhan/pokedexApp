ab29bd41da4f8db27a228d50db9a5165a9cb5519
Etec 244 <e244ti@outlook.com>
2021-08-13 14:42:13 -0300

    Adicionei os componentes Alert e Toast

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,38 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(public alertController: AlertController, public toastController: ToastController) {}
+
+  async exibirAlertaFavorito() {
+    const alert = await this.alertController.create({
+      header: 'Alerta',
+      message: 'Deseja favoritar o filme?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Siiim, favoritar!!!',
+          handler: () => {
+            this.apresentarToast()
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  async apresentarToast() {
+    const toast = await this.toastController.create({
+      message: 'Pokémon adicionado aos favoritos!',
+      duration: 2000,
+      color: 'primary'
+    });
+    toast.present();
+  }
 
 }
